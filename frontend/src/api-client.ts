841d7f58c56431d84af2f8145d8e0718949c30fc
Anthony Patton <anthonyalanpatton@gmail.com>
2024-06-10 21:19:08 -0700

    Added changes to SignIn and Header

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -45,5 +45,17 @@ export const validateToken = async () => {
   if (!response.ok) {
     throw new Error('Token invalid');
   }
+
   return response.json();
+};
+
+export const signOut = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/logout`, {
+    credentials: 'include',
+    method: 'POST',
+  });
+
+  if (!response.ok) {
+    throw new Error('Error during sign out');
+  }
 };
